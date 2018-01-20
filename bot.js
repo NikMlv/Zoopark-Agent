0d330890ac696dd753c2e3da07a0b3488a1313d1
X-495 <35427055+X-495@users.noreply.github.com>
2018-01-20 16:05:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,14 +9,6 @@ client.on('message', message => {
     if (message.content === 'jv!привет') {
     	message.reply('привет!');
   	}
-});
-
-
-client.on('message', message => {
-    if (message.content === 'jv!пока') {
-    	message.reply('ну пока((((9(') {
-  	}
-});
 
     // не убирать это блять
 client.login(process.env.BOT_TOKEN);
