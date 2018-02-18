4c46be559b42970075af53d5e5af0e3c647f44bf
X-495 <35427055+X-495@users.noreply.github.com>
2018-02-18 23:40:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const rule = {creator: "401720942818099200"}
-const creators = ['207821802431315968', '356456653916340224']
-
-
-function declOfNum(number, titles) {  
-    cases = [2, 0, 1, 1, 1, 2];  
-    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
-}
-
 
 client.on("guildMemberAdd", member => {
   const embed = new Discord.RichEmbed()
-  .setColor("#ee83ac")
+  .setColor(65280)
   .setTitle("Добро пожаловать")
-  .setDescription("\nПриветствую тебя на сервере ZOOPARK!\nЭто официальный дискорд-сервер **Happy Wolf'a**.\n\nПожалуйста, прочти и соблюдай правила, которые ты сможешь найти в **#rules**, как и описание ролей сервера.\n\nОбязательно проверяй #updates, ведь там можно найти много чего интерсного.\n\nГлавное будь дружелюбным по отношению к другим участникам, а также соблюдай правила, и тогда никаких проблем не будет. \n\n**Удачи!**") 
-  .setFooter("ZOOPARK")
-  .setThumbnail("https://cdn.discordapp.com/attachments/332255338805854208/411963427972579328/neon231.png")
+  .setDescription("\nПриветствую тебя на сервере ZOOPARK!\nЭто официальный дискорд-сервер **Happy Wolf'a**.\n\nПожалуйста, прочти и соблюдай правила, которые ты сможешь найти в **#rules**, как и описание ролей сервера.\n\nОбязательно проверяй #updates, ведь там можно найти много чего интерсного.\n\nГлавное будь дружелюбным по отношению к другим участникам, а также соблюдай правила, и тогда никаких проблем не будет. \n\n**Удачи!**")
+  .setFooter("JonedVoice")
+  .setThumbnail("https://cdn.discordapp.com/attachments/404577060292526080/414409956024909825/unnamed.png")
   .setTimestamp()
   member.send({embed});
 });
 client.on("message", message => {
-	if (message.channel.id == '411516585887531019') {
-      	message.react("👍");
-      	message.react("👎");
-	}
-
 	if(message.author.bot) return;
 	if(message.content.indexOf(process.env.PREFIX) !== 0) return;
 
@@ -34,51 +20,20 @@ client.on("message", message => {
 
 	if (command == 'test_greetings') {
 	  const embed = new Discord.RichEmbed()
-	  .setColor("#ee83ac")
-	  .setDescription("\nПриветствую тебя на сервере ZOOPARK!\nЭто официальный дискорд-сервер **Happy Wolf'a**.\n\nПожалуйста, прочти и соблюдай правила, которые ты сможешь найти в **#rules**, как и описание ролей сервера.\n\nОбязательно проверяй #updates, ведь там можно найти много чего интерсного.\n\nГлавное будь дружелюбным по отношению к другим участникам, а также соблюдай правила, и тогда никаких проблем не будет. \n\n**Удачи!**") 
-	  .setFooter("JonedVoice")
-	  .setThumbnail("https://cdn.discordapp.com/attachments/332255338805854208/411963427972579328/neon231.png")
+	  .setColor(65280)
+	  .setTitle("Добро пожаловать")
+	  .setDescription("\nПриветствую тебя на сервере ZOOPARK!\nЭто официальный дискорд-сервер **Happy Wolf'a**.\n\nПожалуйста, прочти и соблюдай правила, которые ты сможешь найти в **#rules**, как и описание ролей сервера.\n\nОбязательно проверяй #updates, ведь там можно найти много чего интерсного.\n\nГлавное будь дружелюбным по отношению к другим участникам, а также соблюдай правила, и тогда никаких проблем не будет. \n\n**Удачи!**")
+	  .setFooter("ZOOPARK")
+	  .setThumbnail("https://cdn.discordapp.com/attachments/404577060292526080/414409956024909825/unnamed.png")
 	  .setTimestamp()
 	  message.author.send({embed});
-	} else if (command === "say" && message.author.id == "207821802431315968") {
+	}
+
+	if(command === "say" && message.author.id == "168255014282854401" || "207821802431315968") {
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
-    message.channel.send(sayMessage).catch(O_o=>{message.reply('что-то не так');});
-  	} else if (command === "clear") {
-<<<<<<< HEAD
-  		if(!message.member.roles.some(r=>[rule.creator].includes(r.id)) || !creators.includes(message.member.id))
-=======
-  		if(!message.member.roles.some(r=>[rule.creator].includes(r.id)) or !message.member)
->>>>>>> b430624074b1d8c5e439a381de62aac19e2044a1
-  			return message.reply("Прав нету");
-		message.delete();
-		let content = message.content.slice(process.env.PREFIX.length + 8);
-		let messagecount = parseInt(args[0])+1;
-		let msc = messagecount -1;
-		if (messagecount > 2) {
-			message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-			let lol = declOfNum(msc, ['сообщение', 'сообщения', 'сообщений']);
-			message.channel.send(`Удалено ${msc} ${lol}!`).then(msg => {msg.delete(5000)});
-		} else {
-			message.author.send({embed: {
-				color: 16711680,
-				title: "Ошибка удаления сообщений",
-				description: `\`${content}\` либо меньше двух, либо не является числом.`,
-				footer: {
-				  	text: "ZOOPARK",
-			  	},
-			}});
-		}
-	} else {
-		message.reply({embed: {
-			color: 16711680,
-			title: "Ошибка",
-			description: `Комманды \`${command}\` нету!`,
-			footer: {
-			  	text: "ZOOPARK",
-		  	},
-		}})
-	}
+    message.channel.send(sayMessage);
+  	}
 });
 
 client.login(process.env.BOT_TOKEN);
