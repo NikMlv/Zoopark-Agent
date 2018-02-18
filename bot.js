2600cd508e2bd45d6722c67cf0cde0e4da438daf
X-495 <35427055+X-495@users.noreply.github.com>
2018-02-18 23:41:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on("message", message => {
 	  message.author.send({embed});
 	}
 
-	if(command === "say" && message.author.id == "168255014282854401" || "207821802431315968") {
+	if(command === "say" && message.author.id == "168255014282854401" , "207821802431315968") {
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
