59cecb7fd0ecbbebbbd0763094efc7a6f2108aa6
X-495 <35427055+X-495@users.noreply.github.com>
2018-02-22 14:45:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,10 @@ const client = new Discord.Client();
 
 client.on("guildMemberAdd", member => {
   const embed = new Discord.RichEmbed()
-  .setColor(65280)
+  .setColor(8126719)
   .setTitle("Добро пожаловать")
   .setDescription("\nПриветствую тебя на сервере ZOOPARK!\nЭто официальный дискорд-сервер **Happy Wolf'a**.\n\nПожалуйста, прочти и соблюдай правила, которые ты сможешь найти в **#rules**, как и описание ролей сервера.\n\nОбязательно проверяй #updates, ведь там можно найти много чего интерсного.\n\nГлавное будь дружелюбным по отношению к другим участникам, а также соблюдай правила, и тогда никаких проблем не будет. \n\n**Удачи!**")
-  .setFooter("JonedVoice")
+  .setFooter("ZOOPARK")
   .setThumbnail("https://cdn.discordapp.com/attachments/404577060292526080/414409956024909825/unnamed.png")
   .setTimestamp()
   member.send({embed});
@@ -20,7 +20,7 @@ client.on("message", message => {
 
 	if (command == 'test_greetings') {
 	  const embed = new Discord.RichEmbed()
-	  .setColor(65280)
+	  .setColor(8126719)
 	  .setTitle("Добро пожаловать")
 	  .setDescription("\nПриветствую тебя на сервере ZOOPARK!\nЭто официальный дискорд-сервер **Happy Wolf'a**.\n\nПожалуйста, прочти и соблюдай правила, которые ты сможешь найти в **#rules**, как и описание ролей сервера.\n\nОбязательно проверяй #updates, ведь там можно найти много чего интерсного.\n\nГлавное будь дружелюбным по отношению к другим участникам, а также соблюдай правила, и тогда никаких проблем не будет. \n\n**Удачи!**")
 	  .setFooter("ZOOPARK")
@@ -29,7 +29,7 @@ client.on("message", message => {
 	  message.author.send({embed});
 	}
 
-		if(command === "say" && message.author.id == "325390946583838720","207821802431315968") {
+		if(command === "say" && (message.author.id == "168255014282854401" || message.author.id == "207821802431315968")) {
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
