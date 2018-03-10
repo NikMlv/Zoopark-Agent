025d1c28904b5273041b5eb9110c05a6aa78f779
MeowGMS <35658722+MeowGMS@users.noreply.github.com>
2018-03-10 12:15:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,7 +102,7 @@ client.on('message', message => {
             .addField("Полный никнейм: ", message.author.tag, false)
             .addField("Создан: ", message.author.createdAt, false)
             .addField("Аккаунт верифицирован? ", verified, false)
-           // .addField("Создан: ", message.author.createdAt, false);
+            .addField("Joined At", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
         //.addField("Вы присоеднилсь к серверу: ", message.guild.joinedAt, false)
         .setThumbnail(avatar)
             .setFooter("ZOOPARK")
