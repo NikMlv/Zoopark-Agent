cea5214cabe1706b66ab8ca8b88430c04ef15065
MeowGMS <35658722+MeowGMS@users.noreply.github.com>
2018-03-11 18:54:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,7 +80,7 @@ client.on("message", message => {
     if ((command === "nicktest") && (message.author.id == "356456653916340224")) {
         message.delete().catch(O_o => {});
         message.channel.send(message.author.username);
-        if ("Привет, мир".indexOf("мир") != -1) {
+        if (message.author.username.indexOf("GMS") != -1) {
             message.channel.send("миау");
         }
     }
